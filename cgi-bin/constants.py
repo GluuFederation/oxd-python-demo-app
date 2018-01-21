e8f85bcdbed7826a7cb1a92b6fc7ce7dff4cc3bd
Mike Schwartz <mike@gluu.org>
2018-01-21 13:44:41 -0600

    improved setup

--- cgi-bin/constants.py
+++ cgi-bin/constants.py
@@ -1,24 +1,25 @@
 # Folders
-CGI_BIN = "/usr/lib/cgi-bin"
+CGI_BIN = '/usr/lib/cgi-bin'
+DONT_INSTALL_LIST = ['setupDemo.py']
 
 # Make sure these files are writable by the web server
-APP_FOLDER = "/var/log/sampleapp"
-DB_FILENAME = "%s/sessionDB" % APP_FOLDER
+APP_FOLDER = '/var/log/sampleapp'
+DB_FILENAME = '%s/sessionDB' % APP_FOLDER
 CONFIG_FILE_ORIGINAL = 'demosite.cfg'
 CONFIG_FILE = '%s/demosite.cfg' % APP_FOLDER
 LOG_FN = '%s/app.log' % APP_FOLDER
 
 # Application Preferences
 TITLE = "World's Simplest Web App"
-COOKIE_DOMAIN = "www.example.com"
+COOKIE_DOMAIN = 'www.example.com'
 EXPIRATION_IN_MINUTES = 30
-TZ = "EST+05EDT,M4.1.0,M10.5.0"
+TZ = 'EST+05EDT,M4.1.0,M10.5.0'
 
 # Application URLs
-HOME_URL = "/cgi-bin/home.cgi"
-GET_AUTH_URL = "/cgi-bin/redirect-to-login.cgi"
-GET_LOGOUT_URL = "/cgi-bin/redirect-to-logout.cgi"
-LOGOUT_CONFIRM = "/cgi-bin/logout-confirmation.cgi"
+HOME_URL = '/cgi-bin/home.cgi'
+GET_AUTH_URL = '/cgi-bin/redirect-to-login.cgi'
+GET_LOGOUT_URL = '/cgi-bin/redirect-to-logout.cgi'
+LOGOUT_CONFIRM = '/cgi-bin/logout-confirmation.cgi'
 
 # Resource Server URL
-RS_BASE_URL = "https://localhost:8085/"
+RS_BASE_URL = 'https://localhost:8085/'
--- cgi-bin/setupDemo.py
+++ cgi-bin/setupDemo.py
@@ -1,18 +1,42 @@
 #!/usr/bin/python
 
+# For DEMO purposes ONLY!
+# Not for production usage!
+
 import shelve
 import os
 from constants import *
 from appLog import *
 
+def ls(exclude=[]):
+    import os
+    f_list = []
+    for subdir, dirs, files in os.walk('./'):
+        for file in files:
+            if file not in exclude: f_list.append(file)
+    f_list.sort()
+    return f_list
+
+# Create the database
 db = shelve.open(DB_FILENAME, "n")
 db.close()
-
 log("Database %s created" % DB_FILENAME)
 
-# Not for production usage  :-)
+# Copy the default config file to the live location
 os.system("/bin/cp %s %s" % (CONFIG_FILE_ORIGINAL, CONFIG_FILE))
-os.system("/bin/chmod 777 %s" % DB_FILENAME)
-os.system("/bin/chmod 777 %s" % LOG_FN) 
-os.system("/bin/chmod 777 %s" % CONFIG_FILE) 
+log("Installed %s to %s" % (CONFIG_FILE_ORIGINAL,CONFIG_FILE))
 
+# Copy files to cgi-bin
+cgi_files = ls(DONT_INSTALL_LIST)
+for fn in cgi_files:
+    os.system("/bin/cp %s %s" % (fn, CGI_BIN))
+    log("Installed %s to %s" % (fn,CGI_BIN))
+
+# Make sure the web user can write these files.
+# Even the config file needs to be writable, because
+# the oxid returned from oxd will be written there.
+# Note: 777 allows ANY USER to read / write. Bad for
+# security, but this is only a demo.
+os.system("/bin/chmod 777 %s" % DB_FILENAME)
+os.system("/bin/chmod 777 %s" % LOG_FN)
+os.system("/bin/chmod 777 %s" % CONFIG_FILE)
