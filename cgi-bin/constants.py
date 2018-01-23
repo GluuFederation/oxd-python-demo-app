588848025ce6fb59536b61a317eb20cbca876ec6
Arunmozhi <arun@arunmozhi.in>
2018-01-23 22:50:34 +0530

    added deploy.sh

--- cgi-bin/constants.py
+++ cgi-bin/constants.py
@@ -5,8 +5,8 @@ DONT_INSTALL_LIST = ['setupDemo.py', 'demosite.cfg', 'cleanupDemo.py']
 # Make sure these files are writable by the web server
 APP_FOLDER = '/var/log/sampleapp'
 DB_FILENAME = '%s/sessionDB' % APP_FOLDER
-CONFIG_FILE_ORIGINAL = 'demosite.cfg'
-CONFIG_FILE = '%s/demosite.cfg' % APP_FOLDER
+CONFIG_FILE_ORIGINAL = 'oxd-demo.cfg'
+CONFIG_FILE = '%s/oxd-demo.cfg' % APP_FOLDER
 LOG_FN = '%s/app.log' % APP_FOLDER
 
 # Application Preferences
