7bfe32b3f79c78816d54709a7d8577668fa447c0
Mike Schwartz <mike@gluu.org>
2018-01-26 10:57:47 -0600

    Removed demosite.cfg

--- cgi-bin/constants.py
+++ cgi-bin/constants.py
@@ -6,7 +6,7 @@ TZ = 'EST+05EDT,M4.1.0,M10.5.0'
 
 # Folders
 CGI_BIN = '/usr/lib/cgi-bin'
-DONT_INSTALL_LIST = ['setupDemo.py', 'demosite.cfg', 'cleanupDemo.py']
+DONT_INSTALL_LIST = ['setupDemo.py', 'cleanupDemo.py']
 
 # Make sure these files are writable by the web server
 APP_FOLDER = '/var/log/sampleapp'
@@ -23,3 +23,20 @@ LOGOUT_CONFIRM = '/cgi-bin/logout-confirmation.cgi'
 
 # Resource Server URL
 RS_BASE_URL = 'https://localhost:8085/'
+
+DEMOSITE_CFG = """
+[oxd]
+host = localhost
+port = 8099
+id =
+
+[client]
+authorization_redirect_uri = https://%s/cgi-bin/callback-login.cgi
+post_logout_redirect_uri = https://%s/cgi-bin/logout-confirmation.cgi
+client_frontchannel_logout_uris = https://%s/cgi-bin/callback-logout.cgi
+claims_redirect_uri = https://%s/cgi-bin/callback-claims.cgi
+client_name = World's Simplest Web App
+contacts = admin@example.com
+scope = openid,email,profile
+grant_types = client_credentials,authorization_code
+"""
